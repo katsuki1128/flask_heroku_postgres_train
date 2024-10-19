1fb9c12e53ad0300d13e94f03f9003ef780841a4
katsuki1128 <katsuki1128@gmail.com>
2024-10-19 17:26:25 +0900

    connect

--- app.py
+++ app.py
@@ -10,8 +10,12 @@ app = Flask(__name__)
 
 # HerokuのPostgreSQLデータベースURLを設定（環境変数を使用して安全に取得可能）
 # app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+# app.config["SQLALCHEMY_DATABASE_URI"] = (
+#     "postgresql://udeh3ob4qehaoq:pf6aec7f90ddbfe0023612e8fbaf30a2ca0264155ff950f5fce8713e358f07124@c97r84s7psuajm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d543itvghq5f5j"
+# )
+
 app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "postgresql://udeh3ob4qehaoq:pf6aec7f90ddbfe0023612e8fbaf30a2ca0264155ff950f5fce8713e358f07124@c97r84s7psuajm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d543itvghq5f5j"
+    "postgresql://u338a7ka27i73q:p7320f0260cfc56fdf05d5df29ae336ab62e38bafb5af4ab2be45dc9606641f2a@cat670aihdrkt1.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dmkcc4snini1q"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
